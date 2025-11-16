08bb0455591fbfb7bdc7f575cfc6fc3e4f962731
Matheus Oliveira Vidal <matheus_vidal@outlook.com>
2025-11-15 22:07:57 -0300

    feat: add request tracking with TrackId

---
+++ src/DocBuilder.Domain/Constants/StorageConstants.cs
@@ -0,0 +1,7 @@
+namespace DocBuilder.Domain.Constants;
+
+public static class StorageConstants
+{
+    public const string TemplatesBucketName = "bkt-templates";
+    public const string ApiLogsBucketName = "bkt-audit-logs";
+}
